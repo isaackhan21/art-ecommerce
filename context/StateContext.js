913492082d7d6f872263c3fe18533a05b2214be1
Isaac Khan <isaackhan21@gmail.com>
2022-06-13 20:08:44 +0100

    Success Page

--- context/StateContext.js
+++ context/StateContext.js
@@ -113,6 +113,9 @@ export const StateContext = ({ children }) => {
         onAdd,
         toggleCartItemQuantity,
         onRemove,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
       }}
     >
       {children}
